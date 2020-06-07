af3459a04cfc84f0cd3a2e0bf6349f884bcb9882
Tao Liu <taliu@google.com>
2020-06-06 23:07:25 -0700

    Add Andes Technology to the license header

--- src/isa/riscv_vector_instr.sv
+++ src/isa/riscv_vector_instr.sv
@@ -1,5 +1,6 @@
 /*
  * Copyright 2020 Google LLC
+ * Copyright 2020 Andes Technology Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
