783d4be80d78aa9b2dca7a6be7531deb4a050c24
BharathNR1030 <65217532+BharathNR1030@users.noreply.github.com>
2020-06-25 20:35:32 +0530

    update pygen_src files (#612)

--- pygen/pygen_src/test/riscv_instr_base_test.py
+++ pygen/pygen_src/test/riscv_instr_base_test.py
@@ -9,10 +9,6 @@ http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-Regression script for RISC-V random instruction generator
-
 """
 
 import sys
@@ -25,5 +21,5 @@ class riscv_instr_base_test:
     def __init__(self):
         pass
 
-    for _ in range(cfg.num_of_test):
+    for _ in range(cfg.num_of_tests):
         riscv_instr_ins.create_instr_list(cfg)
