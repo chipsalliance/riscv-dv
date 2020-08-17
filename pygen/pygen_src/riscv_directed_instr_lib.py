6a377ca26db02b6b21332c5af651fcc7f0c1e40d
aneels3 <b150023ec@nitsikkim.ac.in>
2020-08-17 14:39:41 +0530

    fix Todo of directed_lib

--- pygen/pygen_src/riscv_directed_instr_lib.py
+++ pygen/pygen_src/riscv_directed_instr_lib.py
@@ -16,7 +16,7 @@ from enum import IntEnum, auto
 from pygen_src.riscv_instr_stream import riscv_rand_instr_stream
 from pygen_src.isa.riscv_instr import riscv_instr_ins
 from pygen_src.riscv_instr_gen_config import cfg
-from pygen_src.riscv_instr_pkg import riscv_reg_t
+from pygen_src.riscv_instr_pkg import riscv_reg_t, riscv_pseudo_instr_name_t
 from pygen_src.target.rv32i import riscv_core_setting as rcs
 from pygen_src.riscv_pseudo_instr import riscv_pseudo_instr
 
@@ -50,33 +50,29 @@ class int_numeric_e(IntEnum):
 class riscv_int_numeric_corner_stream(riscv_directed_instr_stream):
     def __init__(self):
         super().__init__()
-        self.num_of_avail_regs = vsc.uint8_t(10)
+        self.num_of_avail_regs = 10
         self.num_of_instr = vsc.rand_uint8_t()
-        self.init_val = vsc.rand_list_t(vsc.rand_bit_t(), sz = 10)
+        self.init_val = vsc.rand_list_t(vsc.rand_bit_t(rcs.XLEN - 1), sz = 10)
         self.init_val_type = vsc.rand_list_t(vsc.enum_t(int_numeric_e), sz =10)
         self.init_instr = []
 
-
-    #TODO
-    
     @vsc.constraint
     def init_val_c(self):
-        #TO DO
-        #solve init_val_type before init_val;
-        self.init_val_type.size in vsc.rangelist(self.num_of_avail_regs)
-        self.init_val.size in vsc.rangelist(self.num_of_avail_regs)
+        # TO DO
+        # solve init_val_type before init_val;
+        self.init_val_type.size == self.num_of_avail_regs
+        self.init_val.size == self.num_of_avail_regs
         self.num_of_instr in vsc.rangelist(vsc.rng(15, 30))
-    
+
     @vsc.constraint
     def avail_regs_c(self):
+        self.avail_regs.size == self.num_of_avail_regs
         vsc.unique(self.avail_regs)
         with vsc.foreach(self.avail_regs, idx = True) as i:
             self.avail_regs[i].not_inside(cfg.reserved_regs)
-            self.avail_regs[i] != 0
-    
+            self.avail_regs[i] != riscv_reg_t.ZERO
 
     def pre_randomize(self):
-        # self.avail_regs = [None] * self.num_of_avail_regs
         pass
 
     def post_randomize(self):
@@ -89,10 +85,8 @@ class riscv_int_numeric_corner_stream(riscv_directed_instr_stream):
             elif self.init_val_type[i] == int_numeric_e.NegativeMax:
                 self.init_val[i] = 1 << (rcs.XLEN - 1)
             self.init_instr[i] = riscv_pseudo_instr()
-            # TODO Issue with PyVSC.
-            # TO be fix after getting support from Pyvsc
             self.init_instr[i].rd = self.avail_regs[i]
-            #self.init_instr[i].pseudo_instr_name = 'LI'
+            self.init_instr[i].pseudo_instr_name = riscv_pseudo_instr_name_t.LI
             self.init_instr[i].imm_str = "0x%0x" % (self.init_val[i])
             self.instr_list.append(self.init_instr[i])
         for i in range(self.num_of_instr):
