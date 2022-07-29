0bc9b5e78ead2cdd297b937fadc7c47ac8acc3ac
Greg Chadwick <gac@lowrisc.org>
2022-07-29 16:53:19 +0100

    Expand CSR instruction constraint functionality
    This introduces a 'gen_all_csrs_by_default' configuration setting that
    will cause randomised CSR instructions to target all implemented CSRs.
    CSRs that can be written are further constrained by 'include_write_reg'.
    
    If 'include_write_reg' is non-empty only the CSRs within
    'include_write_reg' can have CSR write instructions generated for them.
    By default MSCRATCH is the only CSR that will see randomly generated CSR
    writes. Further CSRs can be added either by adding to
    'default_include_csr_write' or via configuration options. The
    'add_csr_write' and 'remove_csr_write' options add and remove CSRs from
    the writeable CSRs.
    
    When 'gen_csr_ro_write' is set CSR writes can be generated to read-only
    CSRs, this bypasses any of the writeable CSR controls explained above.
    
    These features have been added with the aim of not altering existing
    behaviour. If the new 'gen_all_csrs_by_default' option isn't set the
    existing instruction set generation behaviour will remain.

--- src/isa/riscv_csr_instr.sv
+++ src/isa/riscv_csr_instr.sv
@@ -14,20 +14,57 @@
  * limitations under the License.
  */
 
+// This expression is needed in multiple constraints so is expressed as a macro. It could be
+// implemented within a function but the constraint solver is unable to work with function contents.
+// So when used in a constraint it will just try different values until it finds something that
+// satisifies what it requires or it gives up. When the set of writable CSRs is small relative to
+// the set of readable CSRs such failures are common. Instead by using an expression that can be
+// directly used in constraints the solver can intelligently solve to find a useable CSR and there
+// are no constraint failures.
+`define CSR_WRITABLE (csr[11:10] == 2'b11 && allow_ro_write) || \
+  ((include_write_reg.size() > 0) && (csr inside {include_write_reg})) || \
+  ((csr[11:10] != 2'b11) && (include_write_reg.size() == 0))
+
 class riscv_csr_instr extends riscv_instr;
   // Privileged CSR filter
-  static privileged_reg_t    exclude_reg[];
-  static privileged_reg_t    include_reg[];
+  static bit [11:0]  exclude_reg[$];
+  static bit [11:0]  include_reg[$];
+  static bit [11:0]  include_write_reg[$];
 
-  constraint csr_c {
-    if (category == CSR) {
-      if (include_reg.size() > 0) {
-        csr inside {include_reg};
-      }
-      if (exclude_reg.size() > 0) {
-        !(csr inside {exclude_reg});
-      }
+  // When set writes to read-only CSRs can be generated
+  static bit allow_ro_write;
+
+  constraint csr_addr_c {
+    if (include_reg.size() > 0) {
+      csr inside {include_reg};
     }
+    if (exclude_reg.size() > 0) {
+      !(csr inside {exclude_reg});
+    }
+  }
+
+  constraint csr_csrrw {
+    if (instr_name == CSRRW || instr_name == CSRRWI) {
+      `CSR_WRITABLE;
+    }
+  }
+
+  constraint csr_csrrsc {
+    if (instr_name == CSRRS || instr_name == CSRRC) {
+      `CSR_WRITABLE || rs1 == 0;
+    }
+  }
+
+  constraint csr_csrrsci {
+    if(instr_name == CSRRSI || instr_name == CSRRCI) {
+      `CSR_WRITABLE || imm == 0;
+    }
+  }
+
+  constraint order {
+    // Choose a CSR before rs1 and imm values. This ensures read-only accesses to read-only CSRs
+    // with similar probability to other CSR accesses.
+    solve csr before rs1, imm;
   }
 
   `uvm_object_utils(riscv_csr_instr)
@@ -39,10 +76,18 @@ class riscv_csr_instr extends riscv_instr;
   static function void create_csr_filter(riscv_instr_gen_config cfg);
     include_reg.delete();
     exclude_reg.delete();
+
+    allow_ro_write = 1;
+
     if (cfg.enable_illegal_csr_instruction) begin
-      exclude_reg = implemented_csr;
+      exclude_reg = {implemented_csr};
     end else if (cfg.enable_access_invalid_csr_level) begin
-      include_reg = cfg.invalid_priv_mode_csrs;
+      include_reg = {cfg.invalid_priv_mode_csrs};
+    end else if (cfg.gen_all_csrs_by_default) begin
+      allow_ro_write = cfg.gen_csr_ro_write;
+      include_reg = {implemented_csr, custom_csr};
+
+      create_include_write_reg(cfg.add_csr_write, cfg.remove_csr_write, default_include_csr_write);
     end else begin
       // Use scratch register to avoid the side effect of modifying other privileged mode CSR.
       if (cfg.init_privileged_mode == MACHINE_MODE) begin
@@ -54,6 +99,20 @@ class riscv_csr_instr extends riscv_instr;
       end
     end
   endfunction : create_csr_filter
+
+  static function void create_include_write_reg(privileged_reg_t add_csr[], privileged_reg_t remove_csr[], bit [11:0] initial_csrs[$]);
+    include_write_reg.delete();
+
+    foreach (initial_csrs[r]) begin
+      if (!(initial_csrs[r] inside {remove_csr})) begin
+        include_write_reg.push_back(initial_csrs[r]);
+      end
+    end
+
+    foreach (add_csr[r]) begin
+      include_write_reg.push_back(add_csr[r]);
+    end
+  endfunction
 
   virtual function void set_rand_mode();
     super.set_rand_mode();
