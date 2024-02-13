b534184855e320312c5d3ec974142b4b91abbc2d
Alfonso Carballo Boullosa <alfonso@alfonso.got.gaisler.com>
2024-02-13 12:54:12 +0100

    set_rand_mode and convert2asm methods added to riscv_zfa_instr

--- src/isa/riscv_zfa_instr.sv
+++ src/isa/riscv_zfa_instr.sv
@@ -15,15 +15,49 @@
  * limitations under the License.
  */
 class riscv_zfa_instr extends riscv_instr;
-
-  rand bit [2:0]              rm;
-  static riscv_instr_name_t   rs2_cte_instr_name[$];
+  bit                       has_rm = 1'b0;
+  rand f_rounding_mode_t    rm;
+  static riscv_instr_name_t rs2_cte_instr_name[$];
 
   `uvm_object_utils(riscv_zfa_instr)
 
   function new(string name = "");
     super.new(name);
   endfunction : new
+
+  virtual function void set_rand_mode();
+    super.set_rand_mode();
+    case (format) inside
+      R_FORMAT: begin
+        if (instr_name inside {FLEQ_H, FLTQ_H, FLEQ_S, FLTQ_S, FLEQ_D, FLTQ_D, FLEQ_Q, FLTQ_Q,
+                               FMINM_H, FMINM_S, FMINM_D, FMINM_Q, FMAXM_H, FMAXM_S, FMAXM_D, FMAXM_Q, 
+                               FMVP_Q_X, FMVP_D_X}) begin
+          has_rs2 = 1'b1;
+          has_rs1 = 1'b1;
+          has_rd = 1'b1;
+          has_imm = 1'b0;
+        end
+      end
+      I_FORMAT: begin
+        if (instr_name inside {FROUND_H, FROUNDNX_H, FROUND_S, FROUNDNX_S, 
+                              FROUND_D, FROUNDNX_D, FROUND_Q, FROUNDNX_Q}) begin
+          has_rs2 = 1'b0;
+          has_rs1 = 1'b1;
+          has_rd = 1'b1;
+          has_rm = 1'b1;
+          has_imm = 1'b0;
+        end else if (instr_name inside {FCVTMOD_W_D, 
+                                        FLI_H, FLI_S, FLI_D, FLI_Q,
+                                        FMVH_X_D, FMVH_X_Q}) begin // use funct3 (no rm field)
+          has_rs2 = 1'b0;
+          has_rs1 = 1'b1;
+          has_rd = 1'b1;
+          has_imm = 1'b0;
+        end
+      end
+
+    endcase
+  endfunction : set_rand_mode
 
   function void pre_randomize();
     // rand_mode deactivated for instructions that dont use it
@@ -34,14 +68,33 @@ class riscv_zfa_instr extends riscv_instr;
     super.pre_randomize();
   endfunction
 
-  virtual function void set_imm_len();
-    if (!(instr_name inside {SLLI_UW})) begin
-      imm_len = $clog2(XLEN) - 1;
-    end else begin
-      imm_len = $clog2(XLEN);
+  virtual function string convert2asm(string prefix = "");
+    string asm_str_final;
+    string asm_str;
+
+    asm_str = format_string(get_instr_name(), MAX_INSTR_STR_LEN);
+
+    case (format)
+      I_FORMAT : begin // instr rd rs1
+          asm_str_final = $sformatf("%0s%0s, %0s, %0s", asm_str, rd.name(), rs1.name());
+        end
+      R_FORMAT : begin // instr rd rs1
+          asm_str_final = $sformatf("%0s%0s, %0s, %0s", asm_str, rd.name(), rs1.name(), rs2.name());
+        end
+
+      default: `uvm_info(`gfn, $sformatf("Unsupported format %0s", format.name()), UVM_LOW)
+    endcase
+
+    if (asm_str_final == "") begin
+      return super.convert2asm(prefix);
     end
-    imm_mask = imm_mask << imm_len;
-  endfunction : set_imm_len
+
+    if (comment != "") begin
+      asm_str_final = { asm_str_final, " #", comment };
+    end
+
+    return asm_str_final.tolower();
+  endfunction : convert2asm
   
   function bit[6:0] get_opcode();
     case (instr_name) inside
@@ -168,7 +221,6 @@ class riscv_zfa_instr extends riscv_instr;
     end
   endfunction : convert2bin
 
-//TODO
   virtual function bit is_supported(riscv_instr_gen_config cfg);
     return (cfg.enable_floating_point && cfg.enable_zfa_extension &&
            (RV32ZFA inside { supported_isa } || RV64ZFA inside { supported_isa } ) &&
