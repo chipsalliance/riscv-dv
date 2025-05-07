531bbdaa6f86a117645ceb2df54f9153817ed852
Domenic Wüthrich <domenic.wuethrich@axelera.ai>
2025-05-07 07:17:06 +0000

    Add Axelera copyright notice to source header

--- src/riscv_instr_pkg.sv
+++ src/riscv_instr_pkg.sv
@@ -1,6 +1,7 @@
 /*
  * Copyright 2018 Google LLC
  * Copyright 2020 Andes Technology Co., Ltd.
+ * Copyright 2025 Axelera AI BV.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
